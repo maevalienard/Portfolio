7bc1c36a42b9c8f3dfc52750b553fe33ada0d0fb
Maeva Lienard <maeva.lienard@strapi.io>
2023-06-21 16:58:01 +0200

    grid + removed some custom css parts to transform them in tailwind properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      'neutral-dk': "#303030",
+      'neutral-md': '#3D3D3D',
+    }
   },
   plugins: [],
 }
