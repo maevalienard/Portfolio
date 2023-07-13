a3c1961f499a578e334e6de9f1a3b65feb69158f
Maeva Lienard <maeva.lienard@strapi.io>
2023-07-13 16:53:38 +0200

    missing files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     colors: {
       'neutral-dk': '#303030',
-      'neutral-md': '#3D3D3D',
+      'neutral-md': '#5E6964',
       'white': '#FFFFFF',
       'border': '#EBEDEC',
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '976px',
+    },
+    dropShadow: {
+      '3xl': '0 8px 8px rgba(122, 120, 182, 0.10)',
+      '4xl': [
+          '0 35px 35px rgba(122, 120, 182, 0.25)',
+          '0 45px 65px rgba(122, 120, 182, 0.15)'
+      ]
     }
   },
   plugins: [],
